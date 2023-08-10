4b72ab89e986bd32c1dffd33901568beba23b5e1
Kofoworola <kofoworolafatogun68@gmail.com>
2023-08-10 10:52:18 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - A program that prints with the use of printf function
+ * main - A program printing with the use of printf function
  * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
