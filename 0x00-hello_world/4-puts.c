456ac7175aade6aa1776449963ca5f3726ee61ae
Kofoworola <kofoworolafatogun68@gmail.com>
2023-08-10 10:18:13 -0700

    Hello printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Learning a program 
- * return: 0 (Success)
+ *main - Learning a program 
+ *return: 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
