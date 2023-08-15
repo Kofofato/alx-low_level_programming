1e8a66bb37afeccdb60fd50fb529edd06d557cdc
Kofoworola <kofoworolafatogun68@gmail.com>
2023-08-15 13:00:44 -0700

    Playing with alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - Make alphabet in lowercase
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
