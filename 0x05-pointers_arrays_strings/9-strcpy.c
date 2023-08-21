e9c4b30e86456d1302f5568298132edeb79aef97
Kofoworola <kofoworolafatogun68@gmail.com>
2023-08-21 13:50:26 -0700

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strcpy - Function that copies the string pointed to by src
+ * @dest: Type of string
+ * @src: Type of string
+ * Return: String
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int n = 0;
+	int m = 0;
+
+	while (*(src + n) !='\0')
+	{
+		n++;
+	}
+	for ( ; m < n ; m++)
+	{
+		dest[m] = src[m];
+	}
+dest[n] = '\0';
+return (dest);
+}
